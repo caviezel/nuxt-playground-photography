9cd38d5df899ce90b8fa4e445970d9767f0a124b
Jim <irawan.jemmy@gmail.com>
2023-07-02 10:21:32 +0800

    package: added vue3-toastify for testing out notif on contact us page

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -4,7 +4,8 @@
       <div class="w-full lg:w-2/3 p-5">
         <div class='w-full flex flex-col items-center pt-5 space-y-5'>
           <h1 class="font-bold text-2xl">Contact Us</h1>
-          <FormKit :style="{ width: '400px' }" type="form" @submit="submit" :value="{ name: 'Ben' }">
+          <FormKit id="form1" :style="{ width: '400px' }" type="form" @submit="submit" :value="{ name: 'Ben' }"
+            :incomplete-message="false">
             <FormKit type="text" label="Name" name="name" validation="required|length:3" help="Enter your full name." />
             <FormKit type="date" label="Birthday" name="dob"
               validation="required|date_between:1990-01-01 00:00:00,1999-12-31 23:59:59"
@@ -13,6 +14,9 @@
             <FormKit type="tel" label="Phone Number" name="phoneNo" placeholder="xxx-xxx-xxxx" validation="required"
               help="What are your digits?" />
           </FormKit>
+          <p class="text-justify text-gray-500">
+            Test Counter from Pinia = {{ mainStore.counter }}
+          </p>
         </div>
         <Footer />
       </div>
@@ -21,11 +25,21 @@
 </template>
 
 <script lang="ts" setup>
+import { useMainStore } from '~/store/main'
+import { toast } from 'vue3-toastify';
 import { delay } from '~/lib/extensions/generics';
+import { reset } from '@formkit/core'
+
+const mainStore = useMainStore();
+
+const notify = (msg: string) => toast.success(msg, { position: toast.POSITION.BOTTOM_RIGHT });
 
 const submit = async (fields: any) => {
   await delay(1000)
-  console.log(fields)
+
+  reset('form1', { 'name': 'Ten' })
+
+  notify(JSON.stringify(fields))
 }
 </script>
 
